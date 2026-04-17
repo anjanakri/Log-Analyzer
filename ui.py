18acee5e148817e65067c0aa78fe931bdcfba7c4
anjanakri.work@gmail.com <anjana.work@gmail.com>
2026-04-17 23:23:00 +0530

    Added a UI Script

--- ui.py
+++ ui.py
@@ -1,2 +1,14 @@
 from tkinter import *
 import customtkinter as ctk
+
+app=ctk.CTk()   #main window
+app.title("Log File Analyzer")
+app.geometry("600x400")
+
+#window widget
+label=ctk.CTkLabel(app, text="Log File Analyzer", font=("Ariel", 24))
+label.pack(pady=20)
+
+button=ctk.CTkButton(app, text="Browse file")
+button.pack(pady=10)
+app.mainloop()
